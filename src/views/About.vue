4baf2c58ea5715c9dfc84bb281863976151e2e30
zhangyang <zy343134464@163.com>
2021-10-23 21:55:41 +0800

    tbd

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>about page</h1>
+    {{getTest}}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import {mapGetters} from 'vuex';
+
+export default defineComponent({
+  name: "About",
+  computed:{
+    ...mapGetters(['getTest'])
+  },
+  methods:{}
+});
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <button @click="goabout">go about</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {mapMutations} from 'vuex';
+
+export default defineComponent({
+  name: "Home",
+  methods:{
+    ...mapMutations(['addTest']),
+    goabout():void{
+      this.addTest();
+      this.$router.push('/about')
+    }
+  }
+});
+</script>
